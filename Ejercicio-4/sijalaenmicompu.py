632f4863309acc88570e848c8e200d811b9e4038
fr3yja81 <fr3yja81@icloud.com>
2026-02-26 11:51:42 -0600

    update

--- Ejercicio-4/sijalaenmicompu.py
+++ Ejercicio-4/sijalaenmicompu.py
@@ -2,6 +2,7 @@ print("Iniciar Sesión")
 
 intentos = 0
 acceso = 0
+subMenus = 0
 
 while intentos < 3:
 
@@ -10,27 +11,21 @@ while intentos < 3:
 
     if user == "":
         print("El usuario no puede estar vacío.")
-        intentos += 1
 
     elif password == "":
         print("La contraseña no puede estar vacía.")
-        intentos += 1
 
     elif " " in password:
         print("La contraseña no debe contener espacios.")
-        intentos += 1
 
     elif len(password) < 8:
         print("La contraseña debe tener al menos 8 caracteres.")
-        intentos += 1
 
     elif not any(c.isdigit() for c in password):
         print("La contraseña debe contener al menos un número.")
-        intentos += 1
 
     elif not any(c.isalpha() for c in password):
         print("La contraseña debe contener al menos una letra.")
-        intentos += 1
 
     elif user == "admin" and password == "admin2026":
         print("Acceso concedido")
@@ -39,8 +34,7 @@ while intentos < 3:
     else:
         print("Acceso denegado")
         intentos += 1
-
-    print(f"Intentos: {intentos}")
+        print(f"Intentos: {intentos}")
 
     if intentos >= 3:
         print("Demasiados intentos. Acceso bloqueado.")
@@ -55,11 +49,54 @@ while intentos < 3:
             opcion = int(input("Seleccione una de la opciones anteriores: "))
 
             if opcion == 1:
+                subMenus = 1
                 print("Clasificar número.")
-            
+                while subMenus == 1:
+                    numero = input("Ingrese un número (e para regresar): ")
+                    if numero.isdigit():
+                        numero = int(numero)
+
+                        if numero == 0:
+                            print("Es 0.")
+                        elif numero < 0:
+                            print("Es negativo.")
+                        elif numero > 0:
+                            print("Es positivo.")
+                            
+                        if numero % 2 == 0:
+                            print("Es par.")
+                        else: 
+                            print("Es impar.")
+                        
+                    elif type(numero) == str:
+                            if numero.lower() == "e":
+                                subMenus = 0
+
             elif opcion == 2:
                 print("Categoria de edad y perimsos.")
-            
+                edad = int(input("Ingrese su edad: "))
+
+                if edad <= 12:
+                    print("Categoria: Niñez")
+                    print("No puedes registrarte.")
+                elif edad > 13 and edad < 18:
+                    print("Categoria: Adolecencia")
+                    print("Puedes registrarte con ayuda de un tutor")
+                elif edad >= 18 and edad < 64:
+                    print("Categoria: Adultez")
+                    print("Puedes registrarte sin tutor.")
+                    
+                licencia = str(input("¿Cuentas con licencia?(s/n): "))
+                if licencia == "s":
+                    print("Puede conducir.")
+                elif licencia == "n":
+                    print("No puedes conducir.")
+
+                identificacion = str(input("¿Cuentas con identificación?(s/n): "))
+                if edad >= 21 and identificacion == "s" and licencia == "s":
+                    print("Tienes servico VIP.")
+
+
             elif opcion == 3:
                 print("Calcular tarifa final.")
 
